eb65d9141a1aedde21b92d64c2b3d6cb2c9b42fd
skeerthiga <30342629+skeerthiga@users.noreply.github.com>
2017-07-23 09:53:30 +0530

    Update a.C

--- a.C
+++ a.C
@@ -1,11 +1,15 @@
-#include <stdio.h>
-int main()
-{
-char j;
-printf("Enter a character: ")
-scanf("%c",&j);
-if( (j>='a' && j<='z') || (j>='A' && j<='Z'))
-printf("%c is an alphabet.",j);else
-printf("%c is not an alphabet.",j);
-return 0;
-}
+   
+int main() {  
+    char character;
+
+  printf("Enter a Ch\n");  
+  scanf("%c", &ch);  
+  if((ch >= 'a' && ch <= 'z')||(ch >= 'A' && ch<= 'Z')){
+  printf("%c is an Alphabet\n", ch);  
+  } 
+  else 
+  {  
+  printf("%c is Not an Alphabet\n", ch);  
+  }
+  return 0;  
+  }
