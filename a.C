012526b705a4835c1a7ed328c11ecb5ada2b2f2e
skeerthiga <30342629+skeerthiga@users.noreply.github.com>
2017-07-23 09:40:54 +0530

    Update a.C

--- a.C
+++ a.C
@@ -1,14 +1,11 @@
 #include <stdio.h>
 int main()
 {
-    char j;
-    printf("Enter a character: ");
-    scanf("%c",&j);
-
-    if( (j>='a' && j<='z') || (j>='A' && j<='Z'))
-        printf("%c is an alphabet.",j);
-    else
-        printf("%c is not an alphabet.",j);
-
-    return 0;
+char j;
+printf("Enter a character: ")
+scanf("%c",&j);
+if( (j>='a' && j<='z') || (j>='A' && j<='Z'))
+printf("%c is an alphabet.",j);else
+printf("%c is not an alphabet.",j);
+return 0;
 }
