7fc4c811f438dcf936f8b0b2b9032c217aa1f9ef
skeerthiga <30342629+skeerthiga@users.noreply.github.com>
2017-07-23 11:44:37 +0530

    Update leap.C

--- leap.C
+++ leap.C
@@ -2,21 +2,21 @@
 int main()
 {
 int y=year;
-printf("Enter a y: ");
-scanf("%d",&y);
+printf("Enter a year");
+scanf("%d",&year);
 if(y%4 == 0)
 {
 if( y%100 == 0)
 {
 if( y%400 == 0)
-printf("%d is a leap year.", y);
+printf("%d is a leap year", year);
 else
-printf("%d is not a leap year.", y);
+printf("%d is not a leap year", year);
 }
 else
-printf("%d is a leap year.", y);
+printf("%d is a leap year", year);
 }
 else
-printf("%d is not a leap year.", y);
+printf("%d is not a leap year", year);
 return 0;
 }
