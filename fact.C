0473c1c353b69040b0f0c85ab3220fda6fa3e4a9
skeerthiga <30342629+skeerthiga@users.noreply.github.com>
2017-07-24 14:33:41 +0530

    Update fact.C

--- fact.C
+++ fact.C
@@ -1,19 +1,13 @@
-#include <stdio.h>
+#include<stdio.h>
 int main()
 {
-int j,l;
-unsigned long long factorial = 1;
-printf("Enter an integer");
-scanf("%d",&j);
-if (j<0)
-printf("Factorial of a negative number doesn't exist");
-else
-{
-for(l=1;l<=j;++l)
-{
-factorial*=l;             
-}
-printf("Factorial of %d = %llu",j,factorial);
-}
+int j,l=1,num;
+printf("Enter a number: ");
+scanf("%d",&num);
+for(j=1;j<=num;j++)
+l=l*j;
+printf("Factorial of %d is: %d",num,l);
 return 0;
 }
+
+
