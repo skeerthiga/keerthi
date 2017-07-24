be04acc26534f4c2180011c557fd14c042d5cc67
skeerthiga <30342629+skeerthiga@users.noreply.github.com>
2017-07-24 10:18:26 +0530

    Update odd.C

--- odd.C
+++ odd.C
@@ -5,7 +5,7 @@ int n,num,num2,k,remark;
 printf("\n Enter any two Numbers : ");
 scanf("%d %d",&num1,&num2);
 printf("\n\n The Prime Numbers between the given Two Intervals are . . . \n ");
-for(n=num1; n<=num2; ++n)
+for(n=num1;n<=num2;++n)
 {
 remark=0;
 for(k=2;k<=n/2;k++){
