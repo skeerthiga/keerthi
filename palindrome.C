1cb93de8b431d38a32c35e11b0f653b6f8524741
skeerthiga <30342629+skeerthiga@users.noreply.github.com>
2017-07-24 09:44:24 +0530

    Update palindrome.C

--- palindrome.C
+++ palindrome.C
@@ -1,20 +1,20 @@
 #include<stdio.h>
-int check_palindrome(int num){
-static int reverse_num=0,rem;
+int check_palindrome(int j){
+static int reverse_j=0,rem;
 if(j!=0){
 rem=j%10;
 reverse_j=reverse_j*10+rem;
 check_palindrome(j/10);
 }
-return reverse_num;
+return reverse_j;
 }
 int main(){
-int num, reverse_num;printf("Enter a number: ");
-scanf("%d",&num);
-reverse_num = check_palindrome(num);
-if(num==reverse_num)
-printf("%d is a palindrome number",num);
+int j, reverse_j;printf("Enter a number: ");
+scanf("%d",&j);
+reverse_j = check_palindrome(j;
+if(j==reverse_j)
+printf("%d is a palindrome number",j);
 else
-printf("%d is not a palindrome number",num);
+printf("%d is not a palindrome number",j);
 return 0;
 }
