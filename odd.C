0ba3fc19469b859b089556fde703bdad102a6c05
skeerthiga <30342629+skeerthiga@users.noreply.github.com>
2017-07-24 10:34:03 +0530

    Update odd.C

--- odd.C
+++ odd.C
@@ -1,14 +1,13 @@
- 
 #include <stdio.h>
 int main()
 {
-int i, n, sum=0;
+int j,k,sum=0;
 printf("Enter upper limit: ");
-scanf("%d", &n);
-for(i=1; i<=n; i+=2)
+scanf("%d",&k);
+for(i=1; i<=k; i+=2)
 {
-sum += i;
+sum+=i;
 }
-printf("Sum of odd numbers = %d", sum);
+printf("Sum of odd numbers = %d",sum);
 return 0;
-} 
+}
