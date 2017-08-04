c0b354eb127c1c6d1d19900b7a4a0922d9ebb0a1
skeerthiga <30342629+skeerthiga@users.noreply.github.com>
2017-08-04 16:54:54 +0530

    Update fib.c

--- fib.c
+++ fib.c
@@ -1,17 +1,16 @@
 #include <stdio.h>
 int main()
 {
-int k1=0,k2=1,nextTerm=0,n;
-printf("Enter a positive number");
-scanf("%d",&n);
-printf("Fibonacci Series: %d, %d",k1,k2);
-nextTerm=k1+k2;
-while(nextTerm<=n)
+int i, n, t1 = 0, t2 = 1, nextTerm;
+printf("Enter the number of terms: ");
+scanf("%d", &n);
+printf("Fibonacci Series: ");
+for (i=1;i<=n;++i)
 {
-printf("%d",nextTerm);
-k1=k2;
-k2=nextTerm;
-nextTerm=k1+k2;
+printf("%d, ", t1);
+nextTerm=t1+t2;
+t1=t2;
+t2=nextTerm;
 }
 return 0;
 }
